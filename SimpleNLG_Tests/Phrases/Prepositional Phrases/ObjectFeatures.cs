32a8222a2327c64b9697e1f43dfc5be4493f1c1e
Terminal Velocity <34872230+GregWickham@users.noreply.github.com>
2020-11-26 09:48:56 -0700

    Add files via upload

---
+++ SimpleNLG_Tests/Phrases/Prepositional Phrases/ObjectFeatures.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleNLG.Tests.PrepositionalPhrases.ObjectFeatures
+{
+    [TestClass]
+    public class CoordinatedObject
+    {
+        [TestMethod]
+        public void OnTheSeasAndOceans()
+        {
+            string realized = Client.Realize(new PPPhraseSpec
+            {
+                Head = Word.Preposition("on"),
+                Complements = new NLGElement[]
+                {
+                    new CoordinatedPhraseElement
+                    {
+                        Conjunction = "and",
+                        Coordinated = new NLGElement[]
+                        {
+                            new NPPhraseSpec
+                            {
+                                Specifier = Word.Determiner("the"),
+                                Head = Word.Noun("sea"),
+                                Number = numberAgreement.PLURAL
+                            },
+                            new NPPhraseSpec
+                            {
+                                Head = Word.Noun("ocean"),
+                                Number = numberAgreement.PLURAL
+                            }
+                        }
+                    }
+                }
+            });
+            Assert.AreEqual("On the seas and oceans.", realized);
+        }
+    }
+}
+
