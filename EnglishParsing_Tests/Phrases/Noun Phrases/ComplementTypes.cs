03f4a76706669ea327e2d74af346eb5fa003f446
Terminal Velocity <34872230+GregWickham@users.noreply.github.com>
2020-11-26 09:50:23 -0700

    Add files via upload

---
+++ EnglishParsing_Tests/Phrases/Noun Phrases/ComplementTypes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlexibleRealization;
+
+namespace EnglishParsing.Tests.NounPhrases.ComplementTypes
+{
+    [TestClass]
+    public class PrepositionalPhrase
+    {
+        [TestMethod]
+        public void TheCatInTheHat() => Assert.AreEqual(
+            "The cat in the hat.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the cat in the hat")));
+    }
+
+    [TestClass]
+    public class SubordinateClause
+    {
+        [TestMethod]
+        public void TheFlyThatLandedOnHisHead() => Assert.AreEqual(
+            "The fly that landed on his head.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the fly that landed on his head")));
+
+        [TestMethod]
+        public void TheLyingLiarWhoLies() => Assert.AreEqual(
+            "The lying liar who lies.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the lying liar who lies")));
+
+        [TestMethod]
+        public void TheYoungPeopleWhoAttendedTheProtest() => Assert.AreEqual(
+            "The young people who attended the protest.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the young people who attended the protest")));
+    }
+}
---
+++ EnglishParsing_Tests/Phrases/Verb Phrases/ComplementTypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlexibleRealization;
+
+namespace EnglishParsing.Tests.VerbPhrases.ComplementTypes
+{
+    [TestClass]
+    public class NounPhrase
+    {
+        [TestMethod]
+        public void TheAuditorExaminedTheBooksCarefully() => Assert.AreEqual("The auditor examined the books carefully.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the auditor examined the books carefully")));
+    }
+
+    [TestClass]
+    public class PrepositionalPhrase
+    {
+        [TestMethod]
+        public void StaringIntoTheSun() => Assert.AreEqual("Staring into the sun.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("staring into the sun")));
+
+
+        [TestMethod]
+        public void RunningAgainstTheDevil() => Assert.AreEqual("Running against the devil.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("running against the devil")));
+    }
+
+    [TestClass]
+    public class NounPhraseAndPrepositionalPhrase
+    {
+        [TestMethod]
+        public void TheProsecutorWillUseTheBooksAsEvidence() => Assert.AreEqual("The prosecutor will use the books as evidence.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the prosecutor will use the books as evidence")));
+    }
+
+
+    [TestClass]
+    public class AdjectivePhrase
+    {
+        [TestMethod]
+        public void TheChildIsHappy() => Assert.AreEqual("The child is happy.",
+            SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("the child is happy")));
+
+        [TestClass]
+        public class WithPrepositionalPhraseComplement
+        {
+            [TestMethod]
+            public void TheShopperIsAngryWithTheHighPrices() => Assert.AreEqual("The shopper is angry with the high prices.",
+                SimpleNLG.Client.Realize(FlexibleRealizerFactory.SpecFrom("The shopper is angry with the high prices")));
+        }
+    }
+}
